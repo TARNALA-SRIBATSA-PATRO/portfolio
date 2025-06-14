1a23e763956fa2810e25f1f3cf5bb18b905ac09a
TARNALA SRIBATSA PATRO <tsribatsapatro@gmail.com>
2025-06-14 16:25:46 +0530

    Update config and add deploy script for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio/", // 👈 Required for GitHub Pages
+  base: "/portfolio/", // 👈 REQUIRED for GitHub Pages
   server: {
     host: "::",
     port: 8080,
